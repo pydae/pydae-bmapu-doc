211f22b8c06af206d1dfbce50d4a7c23b97e10f6
jmmauricio <jmmauricio6@gmail.com>
2022-10-12 00:14:48 +0200

    smib ans k12p6 example added

--- book/conf.py
+++ book/conf.py
@@ -20,7 +20,7 @@ html_favicon = ''
 html_logo = 'logo_bmapu.png'
 html_sourcelink_suffix = ''
 html_theme = 'sphinx_book_theme'
-html_theme_options = {'search_bar_text': 'Search this book...', 'launch_buttons': {'notebook_interface': 'classic', 'binderhub_url': 'https://mybinder.org', 'jupyterhub_url': '', 'thebe': False, 'colab_url': ''}, 'path_to_docs': 'book', 'repository_url': 'https://github.com/pydae/pydae-bmapu-doc', 'repository_branch': 'main', 'google_analytics_id': '', 'extra_navbar': 'Powered by <a href="https://jupyterbook.org">Jupyter Book</a>', 'extra_footer': '', 'home_page_in_toc': True, 'announcement': '', 'use_repository_button': True, 'use_edit_page_button': False, 'use_issues_button': True}
+html_theme_options = {'search_bar_text': 'Search this book...', 'launch_buttons': {'notebook_interface': 'classic', 'binderhub_url': 'https://mybinder.org', 'jupyterhub_url': '', 'thebe': False, 'colab_url': 'https://colab.research.google.com/'}, 'path_to_docs': 'book', 'repository_url': 'https://github.com/pydae/pydae-bmapu-doc', 'repository_branch': 'main', 'google_analytics_id': '', 'extra_navbar': 'Powered by <a href="https://jupyterbook.org">Jupyter Book</a>', 'extra_footer': '', 'home_page_in_toc': True, 'announcement': '', 'use_repository_button': True, 'use_edit_page_button': False, 'use_issues_button': True}
 html_title = 'pydae-bmapu'
 jupyter_cache = ''
 jupyter_execute_notebooks = 'force'
